492e03f3ccc4c3319e4050ce0362fc521ecac299
ravisharmaa <bastola.ravi@gmail.com>
2023-05-07 16:15:47 +0545

    Add feature-tests for the worker controller

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('worker-shift/{workerId}', [ WorkerShiftController::class, 'index'])->name('worker-shift.index');
 Route::post('worker-shift', [ WorkerShiftController::class, 'store'])->name('worker-shift.create');
 Route::controller(WorkerController::class)->group(function() {
-    Route::get('workers', 'index');
-    Route::post('workers', 'store');
-    Route::delete('workers/{workerId}', 'delete');
-    Route::put('workers/{workerId}', 'update');
+    Route::get('workers', 'index')->name('workers.index');
+    Route::post('workers', 'store')->name('workers.create');
+    Route::delete('workers/{workerId}', 'delete')->name('workers.delete');
+    Route::put('workers/{workerId}', 'update')->name('workers.update');
 });
--- tests/Feature/WorkerControllerTest.php
+++ tests/Feature/WorkerControllerTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Worker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class WorkerControllerTest extends TestCase
@@ -13,5 +13,44 @@ class WorkerControllerTest extends TestCase
     public function test_it_should_get_all_the_workers()
     {
         $this->getJson(route('workers.index'))->assertOk();
+    }
+
+    public function test_it_should_validate_worker_creation_request()
+    {
+        $this->postJson(route('workers.create'))->assertUnprocessable();
+    }
+
+    public function test_it_should_create_a_worker()
+    {
+        $worker = Worker::factory()->make()->toArray();
+        $this->postJson(route('workers.create'), $worker)->assertCreated();
+        $this->assertDatabaseCount('workers', 1);
+    }
+
+    public function test_it_should_update_a_worker()
+    {
+        $worker = Worker::factory()->create();
+        $newWorker = Worker::factory()->make()->toArray();
+        $this->putJson(route('workers.update', ['workerId' => $worker->id]), $newWorker)->assertOk();
+        $this->assertDatabaseCount('workers', 1);
+        $this->assertSame($worker->fresh()->name, $newWorker['name']);
+    }
+
+    public function test_it_should_validate_worker_update_request()
+    {
+        $worker = Worker::factory()->create();
+        $this->putJson(route('workers.update', ['workerId' => $worker->id]), [])->assertUnprocessable();
+    }
+
+    public function test_it_should_delete_a_worker()
+    {
+        $worker = Worker::factory()->create();
+        $this->deleteJson(route('workers.delete', ['workerId' => $worker->id]), [])->assertOk();
+        $this->assertDatabaseCount('workers', 0);
+    }
+
+    public function test_it_should_respond_properly_when_a_worker_is_not_found()
+    {
+        $this->deleteJson(route('workers.delete', ['workerId' => '1']), [])->assertBadRequest();
     }
 }
